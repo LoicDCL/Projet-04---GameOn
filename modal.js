b46473b69570d7056bb5a1bf11d7932eccb6a41c
LoicDCL <loic.paingt@gmail.com>
2025-09-04 18:18:03 +0200

    Add files via upload

--- modal.js
+++ modal.js
@@ -21,3 +21,98 @@ function launchModal() {
 }
 
 
+// Fermeture de la modale
+// Déclaration variable fermeture
+const closeBtn = document.querySelector(".close");
+//Mise en place d'un Event Listener
+closeBtn.addEventListener("click", closeModal);
+//fonction qui cache la modale
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+// Message Erreur Champ 01
+// Blocage de l'event si condition non remplie
+document.getElementById("modal-form").addEventListener("submit", function(event){
+  // Variables Erreur Champs 01
+  const firstNameInput = document.getElementById("first");
+  const errorMessage = document.querySelector(".error-first");
+  const value = firstNameInput.value.trim();
+  // Résultat selon conditions THEN - ELSE
+  if (value.length < 2) {
+    event.preventDefault();
+    errorMessage.style.display = "inline";
+  } else {
+    errorMessage.style.display = "none";
+  }
+});
+
+// Message Erreur Champ 02
+// Blocage de l'event si condition non remplie
+document.getElementById("modal-form").addEventListener("submit", function(event){
+  // Variables Erreur Champs 02
+  const lastNameInput = document.getElementById("last");
+  const errorMessage = document.querySelector(".error-last");
+  const value = lastNameInput.value.trim();
+  // Résultat selon conditions THEN - ELSE
+  if (value.length < 2) {
+    event.preventDefault();
+    errorMessage.style.display = "inline";
+  } else {
+    errorMessage.style.display = "none";
+  }
+});
+
+// Message Erreur Email non Valide
+// Blocage de l'event si condition non remplie
+document.getElementById("modal-form").addEventListener("submit", function(event){
+  // Variables Erreur Email
+  const emailInput = document.getElementById("email");
+  const errorMessage = document.querySelector(".error-email");
+  const emailValue = emailInput.value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Résultat selon conditions THEN - ELSE
+  if (!emailRegex.test(emailValue)) {
+    event.preventDefault();
+    errorMessage.style.display = "inline";
+  } else {
+    errorMessage.style.display = "none";
+  }
+});
+
+// Message Erreur sur le nombre d'évènement fait
+// Blocage de l'event si condition non remplie
+document.getElementById("modal-form").addEventListener("submit", function(event){
+  // Variables Erreur quantité
+  const quantityInput = document.getElementById("quantity");
+  const errorMessage = document.querySelector(".error-quantity");
+  const value = quantityInput.value.trim();
+  // Résultat selon conditions THEN - ELSE
+  if (value ==="") {
+    event.preventDefault();
+    errorMessage.style.display = "inline";
+  } else {
+    errorMessage.style.display = "none";
+  }
+});
+
+// Message Erreur sur la non selection checkbox
+// Blocage de l'event si condition non remplie
+document.getElementById("modal-form").addEventListener("submit", function(event){
+  // Variables Erreur non selection
+  const checkboxes = document.getElementsByName("location");
+  const errorMessage = document.querySelector(".error-checkbox-label");
+  // Vérifie si au moins une est cochée
+  let atLeastOneChecked = false;
+  checkboxes.forEach(cb => {
+    if (cb.checked) atLeastOneChecked = true;
+  });
+  // Résultat selon conditions THEN - ELSE
+  if (!atLeastOneChecked) {
+    event.preventDefault();
+    errorMessage.style.display = "inline";
+  } else {
+    errorMessage.style.display = "none";
+  }
+});
